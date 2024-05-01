a5670b5b86a62e757eac3bf93f82c3c4a8011958
GXC 2356 <gxcdev2356@gmail.com>
2024-05-01 00:04:56 +0000

    Add command tools for building
    Building will be much easier yah?

--- tools/build/build.sh
+++ tools/build/build.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+# Thanks to clhex for the script (Github username: clhexftw)
+
 kernel_dir="${PWD}"
 CCACHE=$(command -v ccache)
 objdir="${kernel_dir}/out"
 anykernel=$HOME/anykernel
 builddir="${kernel_dir}/build"
 ZIMAGE=$kernel_dir/out/arch/arm64/boot/Image
-kernel_name="Hiraya"
-zip_name="$kernel_name-$(date +"%d%m%Y-%H%M").zip"
-CLANG_DIR=$HOME/tc/clang-r498229b
+kernel_name="SkylineKernel_vayu_"
+zip_name="$kernel_name$(date +"%Y%m%d").zip"
+CLANG_DIR=tc/clang
 export CONFIG_FILE="vayu_user_defconfig"
 export ARCH="arm64"
+export KBUILD_BUILD_HOST=gxc2356
+export KBUILD_BUILD_USER=home
 
 export PATH="$CLANG_DIR/bin:$PATH"
 
 if ! [ -d "$CLANG_DIR" ]; then
     echo "Toolchain not found! Cloning to $CLANG_DIR..."
-    if ! git clone -q --depth=1 --single-branch https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r498229b.git -b 14.0 $CLANG_DIR; then
+    if ! git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r530567.git -b 14.0 $CLANG_DIR; then
         echo "Cloning failed! Aborting..."
         exit 1
     fi
@@ -60,23 +64,22 @@ completion()
     COMPILED_DTBO=arch/arm64/boot/dtbo.img
     if [[ -f ${COMPILED_IMAGE} && ${COMPILED_DTBO} ]]; then
 
-        git clone -q https://github.com/RecreationRealm/AnyKernel3.git -b master $anykernel
+        git clone -q https://github.com/GXC2356/AnyKernel3.git -b master $anykernel
 
         mv -f $ZIMAGE ${COMPILED_DTBO} $anykernel
 
         cd $anykernel
         find . -name "*.zip" -type f
-        find . -name "*.zip" -type f -delete
+        find . -name "*.zip" -type f
         zip -r AnyKernel.zip *
         mv AnyKernel.zip $zip_name
         mv $anykernel/$zip_name $HOME/$zip_name
         rm -rf $anykernel
-        END=$(date +"%s")
-        DIFF=$(($END - $START))
-        echo -e ${RED} "############################################"
-        echo -e ${RED} "##  BUILD COMPLETED!!! UPLOADING... ##"
-        echo -e ${RED} "############################################${NC}"
+        echo -e ${LGR} "#### build completed successfully (hh:mm:ss) ####"
         exit 0
+    else
+        echo -e ${LGR} "#### failed to build some targets (hh:mm:ss) ####"
+
     fi
 }
 make_defconfig
