012d70c90f25b379c44ebec8226682433121e0fc
muhdhaniff55 <44047948+muhdhaniff55@users.noreply.github.com>
2024-09-30 17:06:15 +0800

    Update packages.sh

--- tools/build/packages.sh
+++ tools/build/packages.sh
@@ -2,4 +2,5 @@
 
 sudo apt-get update
 sudo apt-get install -y build-essential bc lld gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi libssl-dev libfl-dev curl git ftp lftp wget libarchive-tools ccache python2 python2-dev zip unzip tar gzip bzip2 rar unrar cpio jq llvm
+sudo apt-get install -y libelf-dev 
 echo "Installing packages has been done"
